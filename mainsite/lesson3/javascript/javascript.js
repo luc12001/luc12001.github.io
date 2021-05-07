8b814b9c6909d3060eb7675215e2bd61f97fec01
luc12001 <luc12001@byui.edu>
2021-05-07 09:25:41 -0500

    commit w03

---
+++ mainsite/lesson3/javascript/javascript.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+var myDate = new Date();
+var myYear = myDate.getFullYear();
+document.getElementById("theYear").textContent = myYear;
+document.getElementById("lastUpdated").textContent = "Last Updated: " + document.lastModified;
+}
